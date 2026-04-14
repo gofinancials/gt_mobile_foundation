23c0a7a27b03a445eed6eb155e3bfc2bebfdcf0f
Dirisu Jesse <dirisujesse@gmail.com>
2026-04-14 18:57:46 +0100

    [FEAT]: luminance computation and fixes hexColor getter

--- lib/extensions/color_extensions.dart
+++ lib/extensions/color_extensions.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:gt_mobile_foundation/foundation.dart';
+
+// ignore_for_file: deprecated_member_use
 
 extension ColorExtension on Color {
-  String get asCssHexString {
-    final boundaryPattern = RegExp("[Color,(,),0x]");
-    String colorString = toString();
-    colorString = colorString.replaceAll(boundaryPattern, "").trim();
-    final opacity = colorString.substring(0, 2);
-    String color = colorString.substring(2);
-
-    if (color.length == 3) {
-      color = "${color[0] * 2}${color[1] * 2}${color[2] * 2}";
-    }
-
-    return "#$color$opacity";
-  }
-
   Color setOpacity(double opacity) {
     return withValues(alpha: opacity);
+  }
+
+  ColorSet get colorSet => ColorSet(value);
+
+  // A color is considered bright if its luminance is greater than 0.5. This is very expensive to comput so avoid uisng it in performance critical code.
+  bool get isBright => computeLuminance() > 0.5 ? true : false;
+
+  String get asCssHex {
+    final red = r.round().toRadixString(16).padLeft(2, '0');
+    final green = g.round().toRadixString(16).padLeft(2, '0');
+    final blue = b.round().toRadixString(16).padLeft(2, '0');
+    final alpha = a.round().toRadixString(16).padLeft(2, '0');
+    String hex = '#$red$green$blue';
+    if (alpha != 'ff') hex += alpha;
+
+    return hex.toUpperCase();
   }
 }
